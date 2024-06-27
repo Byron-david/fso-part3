ebc4fe78e96a8f3a1ed68b29fdce8651924d81cb
Byron David <byrondavid87@gmail.com>
2024-06-27 10:44:18 -0700

    part3 deployment

---
+++ part3/test.js
@@ -0,0 +1,22 @@
+
+let notes = [
+    {
+      id: 1,
+      content: "HTML is easy",
+      important: true
+    },
+    {
+      id: 2,
+      content: "Browser can execute only JavaScript",
+      important: false
+    },
+    {
+      id: 3,
+      content: "GET and POST are the most important methods of HTTP protocol",
+      important: true
+    }
+  ]
+
+const note = notes.find(note => note.id === 1)
+
+console.log(Math.max(...notes.map(n => Number(n.id))))
